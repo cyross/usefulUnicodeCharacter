cd40b6915675160492085cc95a5b35ee4df07c6c
Cyross Makoto <takuan45189@gmail.com>
2024-11-20 15:34:18 +0900

    [CHANGE]ハングルを初声・母音・終声に分ける

--- src/data/characters.ts
+++ src/data/characters.ts
@@ -66,38 +66,20 @@ function getArabicLetters(): string[] {
   return letters;
 }
 
-// ハングルの配列を生成
-function generateHangul(): string[] {
-  const hangul: string[] = [];
+// 初声（Consonants）
+const hangulInitialConsonants = [
+  'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+];
 
-  // 初声（Consonants）
-  const initialConsonants = [
-    'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
-  ];
+// 母音（Vowels）
+const hangulVowels = [
+  'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'
+];
 
-  // 母音（Vowels）
-  const vowels = [
-    'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ'
-  ];
-
-  // 終声（Final Consonants）
-  const finalConsonants = [
-    '', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
-  ];
-
-  // 基本ハングル（音節）を生成する
-  for (let initial of initialConsonants) {
-    for (let vowel of vowels) {
-      for (let final of finalConsonants) {
-        // ハングルの音節を合成
-        const hangulChar = initial + vowel + final;
-        hangul.push(hangulChar);
-      }
-    }
-  }
-
-  return hangul;
-}
+// 終声（Final Consonants）
+const hangulFinalConsonants = [
+  'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'
+];
 
 const ipaSymbols: string[] = [
   // 子音
@@ -603,7 +585,9 @@ export const characterData: JsonDataMapInterface = {
   ],
   "リング符号付き": ringedCharacters,
   "アラビア文字": getArabicLetters,
-  "ハングル": generateHangul,
+  "ハングル(初声)": hangulInitialConsonants,
+  "ハングル(母音)": hangulVowels,
+  "ハングル(終声)": hangulFinalConsonants,
   "発音記号": ipaSymbols,
   "合字": ligaturesEnglish,
   "ルーン文字": [
